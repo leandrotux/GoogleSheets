63f75e3cfdf7c49ee7e201709a9f2d57d881a039
Leandro <leandrotux@hotmail.com>
2020-10-18 06:07:37 -0300

    Create constants models

---
+++ GoogleSheets/Models/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleSheets.Models
+{
+    public class Constants
+    {
+        public const string ApplicationName = "teste2";
+        public const string SpreadsheetId = "1LCn7Doxyc1P1esyaVKGH4DxoBfnwTG2Gq-FViHxGz_s";
+        public const string Range = "teste!A2:D10";
+        public const string Credentials = "Credentials.json";
+    }
+}
